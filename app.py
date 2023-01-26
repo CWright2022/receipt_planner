e088dd555d6627b9394a4e9d8b51de225805c708
Cayden Wright <mindstorm89@gmail.com>
2023-01-26 20:29:43 +0000

    web app working with gunicorn now

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # importing Flask and other modules
 from flask import Flask, request, render_template
-from test_function import write_to_file
+import adafruit_thermal_printer
+import serial
+
+#thermal printer setup
+uart = serial.Serial("/dev/serial0", baudrate=19200, timeout=3000)
+ThermalPrinter = adafruit_thermal_printer.get_printer_class(2.68)
+printer = ThermalPrinter(uart)
 
 # Flask constructor
 app = Flask(__name__)
@@ -14,7 +20,8 @@ def name():
     if request.method == "POST":
         # getting message from HTML form
         message = request.form.get("message")
-        write_to_file("test_file_out", message)
+        printer.print(message)
+        
 
     return render_template("index.html")
 
