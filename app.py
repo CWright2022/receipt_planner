ec5362f7d0d3b3f75d31cf6bee30a64406a50c64
Cayden Wright <mindstorm89@gmail.com>
2023-01-28 05:53:49 +0000

    resiliency on counting files

--- app.py
+++ app.py
@@ -22,13 +22,25 @@ def increment_counts():
         file.write(new_count)
 
 def get_hourly_count():
-    with open("./hourly_count.txt", "r") as file:
-        count=int(file.readline())
+    #try to get hourly count. if file doesn't exist, make a new one
+    try:
+        with open("./hourly_count.txt", "r") as file:
+            count=int(file.readline())
+    except FileNotFoundError:
+        with open("./hourly_count.txt", "w") as file:
+            file.write("0")
+            count=0
     return count
 
 def get_all_time_count():
-    with open("./all_time_count.txt", "r") as file:
-        count=int(file.readline())
+    #try to get all time count. if file doesn't exist, make a new one
+    try:
+        with open("./all_time_count.txt", "r") as file:
+            count=int(file.readline())
+    except FileNotFoundError:
+        with open("./all_time_count.txt", "w") as file:
+            file.write("0")
+            count=0
     return count
 
 @app.route('/', methods=["GET", "POST"])
@@ -36,15 +48,15 @@ def name():
     messages_this_hour = get_hourly_count()
     if messages_this_hour >=12:
         return render_template("too_many.html")
-    if request.method == "POST":
+    elif request.method == "POST":
         # getting message from HTML form
         name = request.form.get("name")
         message = request.form.get("message")
         print_message(name,message)
         increment_counts()
         return render_template("success.html")
-        
-    return render_template("index.html", all_time_count = get_all_time_count(), hourly_count=messages_this_hour)
+    else:
+        return render_template("index.html", all_time_count = get_all_time_count(), hourly_count=messages_this_hour)
 
 
 if __name__ == '__main__':
--- reset_hourly_count.py
+++
@@ -1,3 +0,0 @@
-#resets the hourly count, this is called by a cron job
-with open("./hourly_count.txt", "w") as file:
-    file.write("0")
