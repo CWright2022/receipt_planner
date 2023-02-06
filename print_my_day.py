9aea688cbd6bb0e2f2e7a262c1bef02e6836eff3
Cayden Wright <mindstorm89@gmail.com>
2023-02-05 23:37:35 -0500

    adjusted file permissions and directory references to work with cron

--- print_my_day.py
+++ print_my_day.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 A cool project that will load all calendar events and print them out
 
@@ -15,7 +16,7 @@ def main():
     printer.print_big_header()
     printer.print_date(date.today())
     # now load and print the calendar
-    events = calendar.get_events_from_file("./calendar_ids.txt")
+    events = calendar.get_events_from_file("/home/pi/receipt_planner/calendar_ids.txt")
     printer.print_events(events)
     # print the weather
     weather_results = weather.get_forecast()
